a173f4606cf54828cb7d1fc9d5813969c26f6632
Melvin-Jovano <melvinjovano2@gmail.com>
2025-02-12 08:52:53 +0700

    feat: count()

--- count.py
+++ count.py
@@ -1,10 +1,34 @@
 import json
 
-with open("./datasets/menus_all.json", "r") as f:
-    menus_data = json.load(f)
+symptoms_data = []
 
-with open("./datasets/menus_all.json", "r") as f:
-    symptoms_data = json.load(f)
+with open("./datasets/menus_train_data.json", "r") as f:
+    menus_data = json.load(f)
+with open("./datasets/menus_val_data.json", "r") as f:
+    menus_data.extend(json.load(f))
+
+with open("./datasets/symptoms_train_data.json", "r") as f:
+    symptoms_data.extend(json.load(f)) 
+with open("./datasets/symptoms_val_data.json", "r") as f:
+    symptoms_data.extend(json.load(f))
+
+
+result = {}
+
+print(len(menus_data))
+print(len(symptoms_data))
 
 for text, annots in menus_data:
-    examples.append(Example.from_dict(nlp.make_doc(text), annots))
+    detected_list = []
+    for a in annots:
+        for i in annots[a]:
+            detected_list.append(i)
+
+            if result.get(i[2]) is None:
+                result[i[2]] = 0
+
+            if i[2] not in detected_list:
+                result[i[2]] += 1
+        # result[a][0] = 1
+
+print(result)
